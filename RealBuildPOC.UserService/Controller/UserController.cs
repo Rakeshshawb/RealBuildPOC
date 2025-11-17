af11eaf597e5f1b1c0b83190c8596e8bfa198fa2
jayantadhar <jayanta.dhar@beas.co.in>
2025-11-17 16:42:26 +0530

    test

--- RealBuildPOC.UserService/Controller/UserController.cs
+++ RealBuildPOC.UserService/Controller/UserController.cs
@@ -19,7 +19,7 @@ namespace RealBuildPOC.UserService.Controller
         [HttpGet]
         public async Task<IActionResult> GetUsers()
         {
-            var users = await _db.QueryAsync<UserDto>("SELECT * FROM [auth].[Users]");
+            var users = await _db.QueryAsync<UserDto>("SELECT * FROM [auth].[Users23]");
             return Ok(users);
         }
     }
